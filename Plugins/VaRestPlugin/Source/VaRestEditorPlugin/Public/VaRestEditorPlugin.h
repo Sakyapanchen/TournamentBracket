d30ab6af042795efaf06a65ba670976b4863cf10
Sakyapanchen <sakyapanchen@gmail.com>
2019-10-31 22:04:09 +0300

    Add project files

---
+++ Plugins/VaRestPlugin/Source/VaRestEditorPlugin/Public/VaRestEditorPlugin.h
@@ -0,0 +1,15 @@
+// Copyright 2014-2019 Vladimir Alyamkin. All Rights Reserved.
+// Original code by https://github.com/unktomi
+
+#pragma once
+
+#include "Modules/ModuleManager.h"
+
+class FVaRestEditorPluginModule : public IModuleInterface
+{
+
+public:
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
---
+++ Source/TournamentBracket/System/TournamentGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintPlatformLibrary.h"
+#include "TournamentGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOURNAMENTBRACKET_API UTournamentGameInstance : public UPlatformGameInstance
+{
+	GENERATED_BODY()
+	
+};
